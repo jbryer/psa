3c0cbe646dcb1c905c1e7536bc7161699323532b
jbryer <jason@bryer.org>
2023-10-23 08:32:24 -0400

    Updated workflows.

--- build.R
+++ build.R
@@ -15,6 +15,19 @@ devtools::check(cran = TRUE)
 # Can run this if there is an error about checking the time
 # Sys.setenv('_R_CHECK_SYSTEM_CLOCK_' = 0)
 
+# Install packages
+install.packages(c('cowplot', 'dplyr', 'ggplot2', 'grid', 'Matching', 'MatchIt',
+    'mice', 'plyr', 'psych', 'remotes', 'reshape2', 'shiny'))
+install.packages(c('knitr', 'lubridate', 'mvtnorm', 'party', 'rmarkdown', 'stargazer'))
+install.packages(c(#'granovaGG',
+				   'bookdown', 'devtools', 'granova', 'GGally',
+				   'multilevelPSA',
+				   # 'PSAboot',
+				   'PSAgraphics', 'rgenoud',
+				   'scholar', 'rbounds', 'tree', 'TriMatch', 'badger', 'BART',
+				   'randomForest', 'stargazer', 'gdata'))
+remotes::install_github('briandk/granovaGG')
+remotes::install_github('jbryer/PSAboot')
 
 ################################################################################
 # For the bookdown site
